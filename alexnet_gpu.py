e67eb7ea0a9cc27148d29e85813f1f9f186e98a5
BetaCat <60107867+timmywanttolearn@users.noreply.github.com>
2021-10-30 21:04:43 +0800

    Update alexnet_gpu.py

--- alexnet_gpu.py
+++ alexnet_gpu.py
@@ -56,41 +56,57 @@ optimizer = optim.Adam(net.parameters(), lr=0.00001)
 # training
 best_acc = 0.0
 save_path = './model.pth'
-for epoch in range(1):
-    # training
+for param in net.parameters():
+    param.requires_grad = False
+net.classifier[-1] = nn.Linear(4096, 10)
+for param in net.classifier.parameters():
+    param.requires_grad = True
+# print(net)
+# define ooptimizer and loss function
+loss_function = nn.CrossEntropyLoss()
+optimizer = optim.Adam(net.parameters(), lr=0.0001)
+best_acc = 0.0
+save_path = './model/alexnet.pth'
+flag = 0
+for epoch in range(20):
+    if epoch>10 and flag ==0:
+        lr = 0.0001
+        fc_param = list(map(id, net.classifier[-1].parameters()))
+        other_param = filter(lambda p: id(p) not in fc_param, net.parameters())
+        optimizer = torch.optim.Adam(
+            [{'params': fc_param}, {'params': other_param, 'lr': lr*0.1}])
+        flag = 1
+    print(epoch)
     net.train()
     running_loss = 0.0
     for step, data in enumerate(trainloader, start=0):
         images, labels = data
         optimizer.zero_grad()
-        logits = net(images.to(device))
-        loss = loss_function(logits, labels.to(device))
+        logits = net(images)
+        loss = loss_function(logits, labels)
         loss.backward()
         optimizer.step()
-
-        # print statistics
         running_loss += loss.item()
-        # print train process
-        rate = (step + 1) / len(trainloader)
+        rate = (step+1)/len(trainloader)
         a = "*" * int(rate * 50)
         b = "." * int((1 - rate) * 50)
-        print("\rtrain loss: {:^3.0f}%[{}->{}]{:.4f}".format(int(rate * 100), a, b, loss), end="")
+        print(
+            "\rtrain loss: {:^3.0f}%[{}->{}]{:.4f}".format(int(rate * 100), a, b, loss), end="")
 
-
-net.eval()  # change into test model
-acc = 0.0  # accumulate accurate number / epoch
-test_num = 0
-with torch.no_grad():
-    for val_data in testloader:
-        val_images, val_labels = val_data
-        outputs = net(val_images.to(device))  # eval model only have last output layer
-        # loss = loss_function(outputs, test_labels)
-        predict_y = torch.max(outputs, dim=1)[1]
-        acc += (predict_y == val_labels.to(device)).sum().item()
-        test_num +=1
-    val_accurate = acc / test_num/batch_size
-    if val_accurate > best_acc:
-        best_acc = val_accurate
-        torch.save(net.state_dict(), save_path)
-    print('[epoch %d] train_loss: %.3f  test_accuracy: %.3f' %
-          (epoch + 1, running_loss / step, val_accurate))
+    net.eval()
+    acc = 0.0
+    test_num = 0
+    with torch.no_grad():
+        for val_data in testloader:
+            val_images, val_labels = val_data
+            outputs = net(val_images)  # eval model only have last output layer
+            # loss = loss_function(outputs, test_labels)
+            predict_y = torch.max(outputs, dim=1)[1]
+            acc += (predict_y == val_labels).sum().item()
+            test_num += 1
+        val_accurate = acc / (test_num * batch_size)
+        if val_accurate > best_acc:
+            best_acc = val_accurate
+            torch.save(net.state_dict(), save_path)
+        print('[epoch %d] train_loss: %.3f  test_accuracy: %.3f' %
+              (epoch + 1, running_loss / step, val_accurate))
